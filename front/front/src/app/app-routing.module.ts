7a49c6b40f37d330b4ff5c269d3eb107d76ca244
MariaRN <mrojanonoval@gmail.com>
2025-04-29 20:11:15 +0200

    commit antes de tocar versiones

--- front/front/src/app/app-routing.module.ts
+++ front/front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
@@ -11,6 +12,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy } // Configuración Hash
+  ]
 })
 export class AppRoutingModule { }
