021983a94befdff66d59bdade5b41cb95e782355
MariaRN <mrojanonoval@gmail.com>
2025-06-11 20:35:09 +0200

    correccion settings

--- back/myappbuela/settings.py
+++ back/myappbuela/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
